feb3adf219464b4b411863b5174a5ca93cc79509
Aravindrvr07 <aravindrvr07@gmail.com>
2024-01-26 16:59:01 +0530

    ARAVIND

--- SeleniumMethods.java
+++ SeleniumMethods.java
@@ -14,7 +14,7 @@ public static void main(String[] args) {
 	
 	WebDriver driver=new ChromeDriver();
 	
-   driver.get("https://www.facebook.com");
+   driver.get("https://phptravels.com/demo/");
    
  //  WebElement ss=driver.findElement(By.id("email"));
   // ss.sendKeys("aravind");
@@ -28,20 +28,51 @@ public static void main(String[] args) {
    // WebElement qq=driver.findElement(By.className("fb_logo _8ilh img"));
    // qq.click();
    
-   WebElement rr=driver.findElement(By.xpath("//a[text()='Create a Page']"));
-    rr.click();
+ //	  WebElement rr=driver.findElement(By.xpath("//a[text()='Create a Page']"));
+   //  rr.click();
     
-    WebElement xx=driver.findElement(By.xpath("//a[@role='button']"));
-    xx.click();
+   // WebElement xx=driver.findElement(By.xpath("//a[@role='button']"));
+   // xx.click();
     
-    WebElement fst=driver.findElement(By.name("firstname"));
-    fst.sendKeys("aravind");
+   // WebElement fst=driver.findElement(By.name("firstname"));
+   // fst.sendKeys("aravind");
     
-    WebElement sur=driver.findElement(By.name("lastname"));
-    sur.sendKeys("rvr");
+    // WebElement sur=driver.findElement(By.name("lastname"));
+    // sur.sendKeys("rvr");
     
-    WebElement mm=driver.findElement(By.xpath("//input[@type='text']"));
-    mm.sendKeys("aravindrrrrr");
+   // WebElement mm=driver.findElement(By.xpath("//input[@type='text']"));
+   // mm.sendKeys("aravindrrrrr");
+   
+   
+   
+   WebElement mm=driver.findElement(By.name("first_name"));
+   mm.sendKeys("aravind");
+   
+   WebElement sm=driver.findElement(By.name("last_name"));
+   sm.sendKeys("aravind");
+   
+   
+   
+   WebElement im=driver.findElement(By.name("business_name"));
+   im.sendKeys("RVR GROUPS");
+   
+   
+
+   WebElement iam=driver.findElement(By.name("email"));
+   iam.sendKeys("aravindrrrrr@gmail.com");
+   
+   WebElement jj=driver.findElement(By.name("demo"));
+   jj.click();
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
     
    
  
